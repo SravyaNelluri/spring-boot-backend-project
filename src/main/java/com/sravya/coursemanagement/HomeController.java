e7196b58654beb065d1ff6250091ba40aba3110e
Sravya <2400031275@kluniversity.in>
2026-04-30 22:10:06 +0530

    Add home route

---
+++ src/main/java/com/sravya/coursemanagement/HomeController.java
@@ -0,0 +1,13 @@
+package com.sravya.coursemanagement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Backend deployed successfully on Render!";
+    }
+}
